df835ab62001e9cc7e7d662d8f7d4e3bb368679b
John C <24364368+lvl-6@users.noreply.github.com>
2021-08-15 15:28:56 +0100

    Changed it to use Navigation Host functionality.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -21,6 +22,11 @@ class TitleFragment : Fragment() {
             container,
             false
         )
+
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+
         return binding.root
     }
 }
